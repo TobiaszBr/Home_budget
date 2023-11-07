e4a8a9a62acd067fd3e76ea14701564e4a8baf66
Tobiasz Bernacki <tobiasz_bernacki@onet.pl>
2023-11-07 17:55:24 +0100

    Add test_retrieve_expense

---
+++ home_budget/expenses/tests/conftest.py
@@ -0,0 +1,52 @@
+import pytest
+from expenses.models import Expense
+
+
+@pytest.fixture
+def client():
+    from rest_framework.test import APIClient
+    return APIClient()
+
+
+@pytest.fixture
+def test_user_password():
+    return "test_password1"
+
+
+@pytest.fixture
+def create_user(django_user_model, test_user_password):
+    def make_user(**kwargs):
+        kwargs["password"] = test_user_password
+        user = django_user_model.objects.create_user(**kwargs)
+        return user
+    return make_user
+
+
+@pytest.fixture
+def auto_login_user(client, create_user, test_user_password):
+    def make_auto_login(user=None):
+        if user is None:
+            user = create_user(username="User1")
+        client.login(username=user.username, password=test_user_password)
+        return client, user
+    return make_auto_login
+
+
+@pytest.fixture
+def valid_expense_data():
+    data = {
+        "category": "Food",
+        "subcategory": "Fast food",
+        "amount": "12.40",
+        "description": "Test description",
+        "date": "2023-01-02"
+    }
+    return data
+
+
+@pytest.fixture
+def expense_model(create_user, valid_expense_data):
+    user = create_user(username="User1")
+    valid_expense_data["user"] = user
+    expense = Expense.objects.create(**valid_expense_data)
+    return expense
--- home_budget/expenses/tests/tests.py
+++ home_budget/expenses/tests/tests.py
@@ -1,30 +1,41 @@
 from rest_framework import status
 from django.urls import reverse
 import pytest
+from expenses.serializers import ExpenseSerializer
 
 
 class TestExpenseView:
     @pytest.mark.django_db
-    def test_create_expense(self, client, create_user, test_user_password):
-        user = create_user(username="Testowy1")
-        client.login(username=user.username, password=test_user_password)
-
+    def test_create_expense(
+            self,
+            auto_login_user,
+            valid_expense_data
+    ):
+        client, user = auto_login_user()
         url = reverse("expense-list")
-        data = {
-            "category": "Food",
-            "subcategory": "Fast food",
-            "amount": 12.3,
-            "description": "Test description",
-        }
-        create_response = client.post(url, data=data, format="json")
-
+        create_response = client.post(url, data=valid_expense_data, format="json")
+        create_response.data.pop("id")
         assert (
                 create_response.status_code == status.HTTP_201_CREATED and
-                create_response.data["category"] == data["category"]
+                create_response.data == valid_expense_data
         )
 
+    @pytest.mark.django_db
+    def test_retrieve_expense(
+            self,
+            django_user_model,
+            auto_login_user,
+            expense_model
+    ):
+        client, user = auto_login_user(user=django_user_model.objects.get())
+        url = reverse("expense-detail", kwargs={"pk": expense_model.id})
+        retrieve_response = client.get(url)
 
-
+        serializer = ExpenseSerializer(expense_model)
+        assert (
+                retrieve_response.status_code == status.HTTP_200_OK and
+                retrieve_response.data == serializer.data
+        )
 
 
 
